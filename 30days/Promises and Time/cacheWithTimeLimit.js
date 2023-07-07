e05aef93014ae125f6f20acc1f0c21c6bd310565
lntt.thuy <lntt.thuy@gmail.com>
2023-07-07 17:01:28 +0700

    push

--- 30days/Promises and Time/cacheWithTimeLimit.js
+++ 30days/Promises and Time/cacheWithTimeLimit.js
@@ -48,4 +48,80 @@ Constraints:
    0 <= duration <= 1000
    total method calls will not exceed 100
 */
+var TimeLimitedCache = function () {
+   this.cache = new Map()
+}
+/** 
+ * @param {number} key
+ * @param {number} value
+ * @param {number} duration time until expiration in ms
+ * @return {boolean} if un-expired key already existed
+ */
+TimeLimitedCache.prototype.set = function (key, value, duration) {
+   const alreadyExists = this.cache.get(key)
+   if(alreadyExists){
+      clearTimeout(alreadyExists.timeoutId)
+   }
+   const timeoutId = setTimeout(() => {
+      this.cache.delete(key)
+   }, duration)
+   this.cache.set(key,{
+      value,timeoutId
+   })
+   return Boolean(alreadyExists)
+}
 
+/** 
+ * @param {number} key
+ * @return {number} value associated with key
+ */
+TimeLimitedCache.prototype.get = function (key) {
+   if (this.cache.has(key))
+      return this.cache.get(key).value
+   return -1
+}
+
+/** 
+ * @return {number} count of non-expired keys
+ */
+TimeLimitedCache.prototype.count = function () {
+   return this.cache.size
+};
+
+
+// --------------------------------------------------------------------------
+
+/**
+ * Your TimeLimitedCache object will be instantiated and called as such: */
+var obj = new TimeLimitedCache()
+obj.set(1, 42, 1000); // false
+console.log("🚀 ~ obj.set(1, 42, 1000):", obj.set(1, 42, 1000));
+obj.get(1) // 42
+console.log("🚀 ~ obj.get(1):", obj.get(1));
+obj.count() // 1
+console.log("🚀 ~ obj.count():", obj.count());
+
+// --------------------------------------------------------------------------
+class _TimeLimitedCache {
+   cache = new Map()
+   set(key,value,duration){
+      const alreadyExists = this.cache.get(key)
+      if(alreadyExists){
+         clearTimeout(alreadyExists.timeoutId)
+      }
+      const timeoutId = setTimeout(() => {
+         this.cache.delete(key)
+      }, duration);
+      this.cache.set(key,{value,timeoutId})
+      return Boolean(alreadyExists)
+   }
+
+   get(key){
+      if(this.cache.has(key)) return this.cache.get(key).value
+      return -1
+   }
+
+   count(){
+      return this.cache.size
+   }
+}
